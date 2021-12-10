8de679ba7fe99ef050f2697af96e280f7676086f
marcoyang <marcoyang1998@gmail.com>
2021-12-10 06:36:31 +0000

    move init

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
+import torch.nn as nn
 
 def draw_trend():
     pass
+
+def _init_weight(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform(m.weight)
+        m.bias.data.fill_(0.01)
